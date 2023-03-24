cd1c3df10e96a89b4e81c217a20872398dffeb84
Samuel Pokam <dixiades@gmail.com>
2023-03-24 22:46:18 +0100

    About.tsx updates

--- components/App.tsx
+++ components/App.tsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/NavBar";
+import About from "@/components/About";
 
 const App = () => {
   return (
-    <div className=" h-full bg-zinc-50">
-      <div className="bg-white ring-1 ring-zinc-100 w-4/5 p-5 mx-auto">
+    <div className="h-full bg-zinc-50">
+      <div className="bg-white ring-1 min-h-screen ring-zinc-100 w-4/5 p-5 mx-auto">
         <NavBar />
+        <About />
       </div>
     </div>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,14 +7,14 @@ const NavBar = () => {
       <div>
         <nav>
           <ul className="mr-5 px-8 text-sm font-medium w-auto gap-5 h-10 bg-white/90 shadow-lg shadow-zinc-800/5 backdrop-blur ring-1 ring-zinc-900/5 rounded-full flex items-center justify-center cursor-pointer">
-            <li className="hover:text-teal-400 transition">About</li>
-            <li className="hover:text-teal-400 transition">Techs</li>
-            <li className="hover:text-teal-400 transition">Projects</li>
-            <li className="hover:text-teal-400 transition">Contact</li>
+            <li className="hover:text-pink-600 transition">About</li>
+            <li className="hover:text-pink-600 transition">Techs</li>
+            <li className="hover:text-pink-600 transition">Projects</li>
+            <li className="hover:text-pink-600 transition">Contact</li>
           </ul>
         </nav>
       </div>
-      <div className="w-12 h-10 text-2xl bg-white/90 shadow-lg shadow-zinc-800/5 backdrop-blur ring-1 ring-zinc-900/5  rounded-full flex items-center justify-center cursor-pointer text-teal-500 hover:text-green-700 transition">
+      <div className="w-12 h-10 text-2xl bg-white/90 shadow-lg shadow-zinc-800/5 backdrop-blur ring-1 ring-zinc-900/5  rounded-full flex items-center justify-center cursor-pointer text-pink-500 hover:text-pink-700 transition">
         <IoSunnyOutline />
       </div>
     </div>
