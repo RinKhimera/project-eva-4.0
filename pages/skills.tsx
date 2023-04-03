e293a43fa3d0d8efdafb3d129297e83efcd9cbbc
Samuel Pokam <dixiades@gmail.com>
2023-04-03 21:37:18 +0100

    assests added

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,16 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 import javascript from "../public/assets/javascript.svg";
 import typescript from "../public/assets/typescript.svg";
+import react from "../public/assets/react.svg";
+import redux from "../public/assets/redux.svg";
 import next from "../public/assets/next.svg";
+import tailwindcss from "../public/assets/tailwindcss.svg";
+import bootstrap from "../public/assets/bootstrap.svg";
+import sass from "../public/assets/sass.svg";
+import d3 from "../public/assets/d3.svg";
 import node from "../public/assets/node.svg";
 import mongodb from "../public/assets/mongodb.svg";
-import tailwindcss from "../public/assets/tailwindcss.svg";
+import postgresql from "../public/assets/postgresql.svg";
+import graphql from "../public/assets/graphql.svg";
+import adobexd from "../public/assets/adobexd.svg";
+import figma from "../public/assets/figma.svg";
 import { useSelector } from "react-redux";
 import { RootState } from "./store";
-import { AiFillLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
-import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { SiExpress, SiNextdotjs } from "react-icons/si";
 
 const Projects = () => {
   const isDarkMode = useSelector(
@@ -20,22 +29,29 @@ const Projects = () => {
   const frontendIcons = [
     {
       href: "#",
-      icon: <Image src={javascript} alt={""} className="w-8 h-8" />,
+      icon: <Image src={javascript} alt={""} className="w-7 h-7" />,
       name: "JavaScript",
     },
     {
       href: "#",
-      icon: <Image src={mongodb} alt={""} className="w-10 h-10" />,
-      name: "MongoDB",
-    },
-    {
-      href: "#",
-      icon: <Image src={typescript} alt={""} className="w-8 h-8" />,
+      icon: <Image src={typescript} alt={""} className="w-7 h-7" />,
       name: "TypeScript",
     },
     {
       href: "#",
-      icon: <Image src={next} alt={""} className="w-8 h-8" />,
+      icon: <Image src={react} alt={""} className="w-8 h-8" />,
+      name: "React",
+    },
+    {
+      href: "#",
+      icon: <Image src={redux} alt={""} className="w-8 h-8" />,
+      name: "Redux",
+    },
+    {
+      href: "#",
+      icon: (
+        <SiNextdotjs className="w-9 h-9 text-zinc-600 dark:text-zinc-400" />
+      ),
       name: "Next.js",
     },
     {
@@ -45,27 +61,59 @@ const Projects = () => {
     },
     {
       href: "#",
+      icon: <Image src={bootstrap} alt={""} className="w-9 h-9" />,
+      name: "Bootstrap",
+    },
+    {
+      href: "#",
+      icon: <Image src={sass} alt={""} className="w-9 h-9" />,
+      name: "Sass",
+    },
+    {
+      href: "#",
+      icon: <Image src={d3} alt={""} className="w-8 h-8" />,
+      name: "D3.js",
+    },
+  ];
+
+  const backendIcons = [
+    {
+      href: "#",
       icon: <Image src={node} alt={""} className="w-8 h-8" />,
       name: "Node.js",
     },
     {
       href: "#",
-      icon: <Image src={next} alt={""} className="w-8 h-8" />,
-      name: "Next.js",
+      icon: <SiExpress className="w-9 h-9 text-zinc-600 dark:text-zinc-400" />,
+      name: "Express.js",
     },
     {
       href: "#",
-      icon: (
-        <AiFillGithub className="w-8 h-8 text-zinc-600 dark:text-zinc-400" />
-      ),
-      name: "GitHub",
+      icon: <Image src={mongodb} alt={""} className="w-10 h-10" />,
+      name: "MongoDB",
     },
     {
       href: "#",
-      icon: (
-        <AiFillGithub className="w-8 h-8 text-zinc-600 dark:text-zinc-400" />
-      ),
-      name: "GitHub",
+      icon: <Image src={postgresql} alt={""} className="w-8 h-8" />,
+      name: "PostgreSQL",
+    },
+    {
+      href: "#",
+      icon: <Image src={graphql} alt={""} className="w-8 h-8" />,
+      name: "GraphQL",
+    },
+  ];
+
+  const designIcons = [
+    {
+      href: "#",
+      icon: <Image src={figma} alt={""} className="w-8 h-8" />,
+      name: "Figma",
+    },
+    {
+      href: "#",
+      icon: <Image src={adobexd} alt={""} className="w-8 h-8" />,
+      name: "AdobeXD",
     },
   ];
 
@@ -90,17 +138,16 @@ const Projects = () => {
             projects={false}
             contact={false}
           />
-          <div className="sm:px-8 mt-16 sm:mt-32">
+          <main className="sm:px-8 mt-16 sm:mt-32">
             <div className="mx-auto max-w-7xl lg:px-8">
               <div className="relative px-4 sm:px-8 lg:px-12">
                 <div className="mx-auto max-w-2xl lg:max-w-5xl">
                   <div className="mt-16 sm:mt-20">
                     <div className="md:border-l md:border-zinc-300 md:pl-6 md:dark:border-zinc-500/40">
                       <div className="flex max-w-3xl flex-col space-y-16">
-                        <article className="md:grid md:grid-cols-4 md:items-baseline">
+                        <section className="md:grid md:grid-cols-4 md:items-baseline">
                           <div className="md:col-span-3 group relative flex flex-col items-start">
                             <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
-
                             <div className="md:hidden relative z-10 order-first mb-3 flex items-center text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 pl-3.5">
                               <span
                                 className="absolute inset-y-0 left-0 flex items-center"
@@ -116,7 +163,7 @@ const Projects = () => {
                                 adipisicing elit. Minima ut quidem accusantium
                                 ratione aliquid corrupti eius sed sint eum.
                               </p>
-                              <div className="grid grid-cols-6 lg:grid-cols-7 gap-x-6 gap-y-6 p-2">
+                              <div className="grid grid-cols-5 md:grid-cols-6 lg:grid-cols-7 gap-x-6 gap-y-6 p-2">
                                 {frontendIcons.map((icon, index) => (
                                   <a
                                     href={icon.href}
@@ -140,11 +187,10 @@ const Projects = () => {
                           <div className="mt-1 hidden md:block relative z-10 order-first mb-3 items-center text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
                             Frontend techs
                           </div>
-                        </article>
-                        <article className="md:grid md:grid-cols-4 md:items-baseline">
+                        </section>
+                        <section className="md:grid md:grid-cols-4 md:items-baseline">
                           <div className="md:col-span-3 group relative flex flex-col items-start">
                             <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
-
                             <div className="md:hidden relative z-10 order-first mb-3 flex items-center text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 pl-3.5">
                               <span
                                 className="absolute inset-y-0 left-0 flex items-center"
@@ -152,7 +198,7 @@ const Projects = () => {
                               >
                                 <span className="h-4 w-0.5 rounded-full bg-zinc-800 dark:bg-zinc-100"></span>
                               </span>
-                              Frontend techs
+                              Backend techs
                             </div>
                             <div className="flex flex-col items-start gap-2">
                               <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
@@ -160,8 +206,8 @@ const Projects = () => {
                                 adipisicing elit. Minima ut quidem accusantium
                                 ratione aliquid corrupti eius sed sint eum.
                               </p>
-                              <div className="grid grid-cols-6 lg:grid-cols-7 gap-x-6 gap-y-6 p-2">
-                                {frontendIcons.map((icon, index) => (
+                              <div className="grid grid-cols-5 md:grid-cols-6 lg:grid-cols-7 gap-x-6 gap-y-6 p-2">
+                                {backendIcons.map((icon, index) => (
                                   <a
                                     href={icon.href}
                                     target="_blank"
@@ -169,7 +215,7 @@ const Projects = () => {
                                     key={index}
                                   >
                                     <div className="flex flex-col items-center gap-1">
-                                      <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                                      <div className="relative z-10 flex h-14 w-14 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                                         {icon.icon}
                                       </div>
                                       <h2 className="relative z-10 text-sm text-zinc-600 dark:text-zinc-400">
@@ -182,16 +228,60 @@ const Projects = () => {
                             </div>
                           </div>
                           <div className="mt-1 hidden md:block relative z-10 order-first mb-3 items-center text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                            Frontend techs
+                            Backend techs
                           </div>
-                        </article>
+                        </section>
+                        <section className="md:grid md:grid-cols-4 md:items-baseline">
+                          <div className="md:col-span-3 group relative flex flex-col items-start">
+                            <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+                            <div className="md:hidden relative z-10 order-first mb-3 flex items-center text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 pl-3.5">
+                              <span
+                                className="absolute inset-y-0 left-0 flex items-center"
+                                aria-hidden="true"
+                              >
+                                <span className="h-4 w-0.5 rounded-full bg-zinc-800 dark:bg-zinc-100"></span>
+                              </span>
+                              Design tools
+                            </div>
+                            <div className="flex flex-col items-start gap-2">
+                              <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Minima ut quidem accusantium
+                                ratione aliquid corrupti eius sed sint eum.
+                              </p>
+                              <div className="grid grid-cols-5 md:grid-cols-6 lg:grid-cols-7 gap-x-6 gap-y-6 p-2">
+                                {designIcons.map((icon, index) => (
+                                  <a
+                                    href={icon.href}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    key={index}
+                                  >
+                                    <div className="flex flex-col items-center gap-1">
+                                      <div className="relative z-10 flex h-14 w-14 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                                        {icon.icon}
+                                      </div>
+                                      <h2 className="relative z-10 text-sm text-zinc-600 dark:text-zinc-400">
+                                        {icon.name}
+                                      </h2>
+                                    </div>
+                                  </a>
+                                ))}
+                              </div>
+                            </div>
+                          </div>
+                          <div className="mt-1 hidden md:block relative z-10 order-first mb-3 items-center text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                            Design tools
+                          </div>
+                        </section>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </main>
+          <Footer />
         </div>
       </div>
     </div>
