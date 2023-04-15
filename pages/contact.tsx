36e3ebd6a0a0736f78cd18d83d8fcde0ff2e8fb9
Samuel Pokam <dixiades@gmail.com>
2023-04-15 16:54:24 +0100

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -88,7 +88,10 @@ const Projects = () => {
                               name="last-name"
                               id="last-name"
                               autoComplete="family-name"
-                              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 
+                              shadow-sm ring-1 ring-inset
+                              ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset 
+                              focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                           </div>
                         </div>
