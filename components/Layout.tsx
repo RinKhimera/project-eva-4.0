d4fb5e50d65e3a6973048674924d5e7b675bcbe7
Samuel Pokam <dixiades@gmail.com>
2023-05-28 01:20:08 +0100

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
 import { RootState } from "@/pages/store"
 import { useSelector } from "react-redux"
+import Navbar from "./Navbar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const isDarkMode = useSelector(
